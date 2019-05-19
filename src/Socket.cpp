91a91c3abde6030fbae3c9434e3fa6e5c2ca48e3
Paul Soucy <paul@Pauls-Air.home.dev-smart.com>
2019-05-19 10:54:40 -0400

    wip

--- src/Socket.cpp
+++ src/Socket.cpp
@@ -1,10 +1,12 @@
 
+#include <arpa/inet.h>
 #include "Socket.h"
 
 namespace plnet
 {
     INetAddress::INetAddress(const string& addrStr, uint16_t port)
     {
-        
+        inet_pton(AF_INET, addrStr.c_str(), &ipv4addr);
+        this->port = port;
     }
 }
--- test/SocketTests.cpp
+++ test/SocketTests.cpp
@@ -3,9 +3,13 @@
 
 #include "Socket.h"
 
+#include <arpa/inet.h>
+
 using namespace plnet;
 
 TEST(INetAddress, parse_string) {
-    INetAddress socket("127.0.0.1", 4000);
+    INetAddress socket("192.168.1.1", 4000);
     
+    EXPECT_EQ(3232235777, ntohl(socket.ipv4addr));
+    EXPECT_EQ(4000, socket.port);
 }
